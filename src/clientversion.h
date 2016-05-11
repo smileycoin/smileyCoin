4d2bebae87f3b39a193e130f98453c85b861cdbe
Myckel Habets <myckel@sdf.lonestar.org>
2016-05-11 23:58:05 +0200

    Push private key import fix to master.

--- src/clientversion.h
+++ src/clientversion.h
@@ -9,7 +9,7 @@
 
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       2016
-#define CLIENT_VERSION_MINOR       03
+#define CLIENT_VERSION_MINOR       05
 #define CLIENT_VERSION_REVISION    1
 #define CLIENT_VERSION_BUILD       0
 
