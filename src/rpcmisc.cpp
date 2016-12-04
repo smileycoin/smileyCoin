6097368b9cd5b3d8fd3782eca559b0262eedf479
ny2cafuse <jessfoddrill@gmail.com>
2016-12-04 09:03:33 -0800

    A few more getinfo changes.
    Adding build name.

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -34,9 +34,10 @@ Value getinfo(const Array& params, bool fHelp)
             "Returns an object containing various state info.\n"
             "\nResult:\n"
             "{\n"
-            "  \"version\": xxxxx,           (numeric) the server version\n"
-            "  \"protocolversion\": xxxxx,   (numeric) the protocol version\n"
+            "  \"build_name\": xxxxx,        (string) the wallet build name\n"
             "  \"build_date\": xxxxx,        (string) the wallet build date\n"
+            "  \"version\": xxxxx,           (numeric) the wallet build version\n"
+            "  \"protocolversion\": xxxxx,   (numeric) the protocol version\n"
             "  \"balance\": xxxxxxx,         (numeric) the total auroracoin balance of the wallet\n"
             "  \"blocks\": xxxxxx,           (numeric) the current number of blocks processed in the server\n"
             "  \"timeoffset\": xxxxx,        (numeric) the time offset\n"
@@ -59,9 +60,10 @@ Value getinfo(const Array& params, bool fHelp)
     GetProxy(NET_IPV4, proxy);
 
     Object obj;
+    obj.push_back(Pair("client_name",      CLIENT_NAME));
+    obj.push_back(Pair("build_date",      CLIENT_DATE));
     obj.push_back(Pair("version",         (int)CLIENT_VERSION));
     obj.push_back(Pair("protocolversion", (int)PROTOCOL_VERSION));
-    obj.push_back(Pair("build_date",      CLIENT_DATE));
 #ifdef ENABLE_WALLET
     if (pwalletMain) {
         obj.push_back(Pair("balance",       ValueFromAmount(pwalletMain->GetBalance())));
