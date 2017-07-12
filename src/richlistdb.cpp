45658e43a634782f01b84d21151b9012da9bcb0a
Dagur Tómas Ásgeirsson <dagurtomas@gmail.com>
2017-07-12 14:36:35 +0000

    Changed loop to while(true)

--- src/richlistdb.cpp
+++ src/richlistdb.cpp
@@ -55,7 +55,7 @@ public:
         }
         unsigned int fFlags = DB_SET_RANGE;
         maxheight = 0;
-        loop
+        while (true)
         {
             // Read next record
             CDataStream ssKey(SER_DISK, CLIENT_VERSION);
