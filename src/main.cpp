ab7cbc4c5d24f0f34bd37e5c44350d60ee8ae1b1
HarriOrri <haraldur1234@gmail.com>
2019-06-11 14:55:57 +0000

    Use nMinTxFee from main

--- src/main.cpp
+++ src/main.cpp
@@ -898,7 +898,7 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransa
 		// See if transaction contains OP_RETURN output
 		if (ContainsOpReturn(tx)){ //
 			// Only allow OP_RETURN transactions that send atleast txMinFee amount
-			if (nValueOut < txMinFee)
+			if (nValueOut < tx.nMinTxFee)
 				return state.DoS(0, error("AcceptToMemoryPool : not enough value sent for OP_RETURN transaction %s, %d < %d", hash.ToString(), nValueOut, txMinFee), REJECT_INSUFFICIENTFEE, "insufficient output value");
 		}
 		
