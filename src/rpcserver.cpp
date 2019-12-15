4cb7b259fd610dfa71f6d27f30e312c70efe7fd9
krimtom <kmt3@hi.is>
2019-12-15 16:32:25 +0000

    Update rpcserver.cpp

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -316,6 +316,9 @@ static const CRPCCommand vRPCCommands[] =
     { "gethashespersec",        &gethashespersec,        true,      false,      false },
     { "getwork",                &getwork,                true,      false,      true  },
     { "setgenerate",            &setgenerate,            true,      true,       false },
+  
+    /* Misc */
+    { "amicool",                &amicool,                true,      false,      false },
 #endif // ENABLE_WALLET
 };
 
