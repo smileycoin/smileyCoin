b9dfe24899aa8939587595abfa331481d28c8d96
Bragi Arnarson <42941290+Bragiar@users.noreply.github.com>
2020-06-25 16:22:22 +0000

    Fix for Boost 1.70+
    Based on https://github.com/cygnusxi/CurecoinSource/commit/6b93af07005b90e4f6c5785c60c10c36fa52a333

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -18,6 +18,18 @@
 #include "json/json_spirit_utils.h"
 #include "json/json_spirit_writer_template.h"
 
+// Boost Support for 1.70+
+#if BOOST_VERSION >= 107000
+    #define GetIOService(s) ((boost::asio::io_context&)(s).get_executor().context())
+    #define GetIOServiceFromPtr(s) ((boost::asio::io_context&)(s->get_executor().context())) /
+    typedef boost::asio::io_context ioContext;
+
+#else
+    #define GetIOService(s) ((s).get_io_service())
+    #define GetIOServiceFromPtr(s) ((s)->get_io_service())
+    typedef boost::asio::io_service ioContext;
+#endif
+
 class CBlockIndex;
 
 /* Start RPC threads */
