22c52a780d87098dc3cfddd66a51c3248c9c5cde
Anton Vilhelm Ásgeirsson <antonva@users.noreply.github.com>
2020-08-26 13:00:25 +0000

    [depends] change bdb preprocess cmds to reflect change in 5.3 tree
    Source files now live in `./src/`

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -14,8 +14,8 @@ $(package)_cppflags_mingw32=-DUNICODE -D_UNICODE
 endef
 
 define $(package)_preprocess_cmds
-  sed -i.old 's/__atomic_compare_exchange/__atomic_compare_exchange_db/' dbinc/atomic.h && \
-  sed -i.old 's/atomic_init/atomic_init_db/' dbinc/atomic.h mp/mp_region.c mp/mp_mvcc.c mp/mp_fget.c mutex/mut_method.c mutex/mut_tas.c && \
+  sed -i.old 's/__atomic_compare_exchange/__atomic_compare_exchange_db/' src/dbinc/atomic.h && \
+  sed -i.old 's/atomic_init/atomic_init_db/' src/dbinc/atomic.h src/mp/mp_region.c src/mp/mp_mvcc.c src/mp/mp_fget.c src/mutex/mut_method.c src/mutex/mut_tas.c && \
   sed -i.old 's/WinIoCtl/winioctl/' src/dbinc/win_db.h && \
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
 endef
