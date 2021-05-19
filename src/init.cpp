6b80af7d91b28d03a9ef7af8d9c009eceaf9e326
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2021-05-19 09:01:31 +0000

    Remove getwork() RPC call

--- src/init.cpp
+++ src/init.cpp
@@ -125,7 +125,6 @@ void Shutdown()
     RenameThread("bitcoin-shutoff");
     mempool.AddTransactionsUpdated(1);
     StopRPCThreads();
-    ShutdownRPCMining();
 #ifdef ENABLE_WALLET
     if (pwalletMain)
         bitdb.Flush(false);
@@ -1275,8 +1274,6 @@ bool AppInit2(boost::thread_group& threadGroup)
 #endif
 
     StartNode(threadGroup);
-    // InitRPCMining is needed here so getwork/getblocktemplate in the GUI debug console works properly.
-    InitRPCMining();
     if (fServer)
         StartRPCThreads();
 
