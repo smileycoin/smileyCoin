f2045edf4c88b7db5b5b6a052506848d21fe2636
Ingimundur <iag10@DESKTOP-C8FFHDL.localdomain>
2022-11-16 18:09:26 +0000

    Name of command added to list of commands. sayhello.

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -226,6 +226,7 @@ static const CRPCCommand vRPCCommands[] =
     /* Overall control/query calls */
     { "getinfo",                &getinfo,                true,      false,      false }, /* uses wallet if enabled */
     { "help",                   &help,                   true,      true,       false },
+    { "sayhello",               &sayhello,               true,      false,      false },
     { "stop",                   &stop,                   true,      true,       false },
 
     /* P2P networking */
